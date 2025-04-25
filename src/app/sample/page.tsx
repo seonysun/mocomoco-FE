6bcf87e6caf14beb6e9a863bebad6aff12f7f3f0
seonysun <csh99240@naver.com>
2025-04-25 12:37:23 +0900

    chore: 라우팅용 샘플 디렉토리 추가

---
+++ src/app/sample/page.tsx
@@ -0,0 +1,12 @@
+export default function HomePage() {
+  return (
+    <main className="p-6">
+      <h1 className="text-main-primary text-2xl font-bold">
+        모두를 위한 모각코, 모코모코!
+      </h1>
+      <p className="text-main-soft mt-4">
+        👩‍💻 혼자보다 함께, 모코모코에서 모각코를 즐겨보세요!
+      </p>
+    </main>
+  );
+}
